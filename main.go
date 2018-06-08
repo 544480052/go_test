3783189a831f19887c8b85f427b2c65997faa170
程曦 <544480052@qq.com>
2018-06-08 17:48:22 +0800

    debug

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ func main() {
 	fmt.Println(timestamp)
 
 	//yyyy-mm-dd时间格式化成时间戳
-	tm1, _ := time.Parse("YYYY-mm-dd HH:ii:ss", "2018-01-01 15:04:05")
+	tm1, _ := time.Parse("2018-01-01 15:04:05", "2018-01-01 15:04:05")
 	fmt.Println(tm1.Unix())
 
 	//时间戳格式化成yyyy-mm-d
 	tm2 := time.Unix(timestamp, 0)
-	fmt.Println(tm2.Format("YYYY-mm-dd HH:ii:ss"))
+	fmt.Println(tm2.Format("2018-01-01 15:04:05"))
 
 }
