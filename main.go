b9adb4a149295d9c7f5ea38402b2a7fdd9356745
程曦 <544480052@qq.com>
2018-06-08 16:56:19 +0800

    时间种子随机数

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"math/rand"
 )
 
 func main()  {
 
-	for i:=0;i<10;i++ {
-		fmt.Println(rand.Intn(100))
+	//for i:=0;i<10;i++ {
+	//	//生成0-100之间的整数随机数
+	//	fmt.Println(rand.Intn(100))
+	//}
+
+	for i:=0;i<10;i++  {
+		fmt.Print(rand.Seed(time.Now().UnixNano()))
 	}
 
 }
