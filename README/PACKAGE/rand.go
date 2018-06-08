9557a6d22eb2d9027bd040409975ffd6e3eb764c
程曦 <544480052@qq.com>
2018-06-08 17:14:22 +0800

    时间

--- README/PACKAGE/rand.go
+++ README/PACKAGE/rand.go
@@ -1,4 +1,3 @@
-package PACKAGE
 /**
 "math/rand"包 实现了伪随机数生成器。也就是生成 整形和浮点型
 该包中根据生成伪随机数是是否有种子(可以理解为初始化伪随机数)，可以分为两类：
@@ -6,4 +5,43 @@ package PACKAGE
 2、无种子。可以理解为此时种子为1， Seed(1)
 */
 
+package main
 
+import (
+"fmt"
+"math/rand"
+"time"
+)
+
+func main()  {
+
+	//for i:=0;i<10;i++ {
+	//	//生成0-100之间的整数随机数
+	//	fmt.Println(rand.Intn(100))
+	//}
+
+	//初始时间化随机数种子
+	//rand.Seed(time.Now().UnixNano())
+	//for i:=0;i<10;i++  {
+	//	fmt.Println(rand.Intn(100))
+	//}
+
+	//初始时间化随机数种子
+	//r:=	rand.New(rand.NewSource(time.Now().UnixNano()))
+	//for i:=0;i<10;i++  {
+	//	fmt.Println(r.Intn(100))
+	//}
+
+
+	for i:=0;i<10;i++  {
+		fmt.Println(rand.Int63())
+	}
+	//初始化时间随机数种子
+	r:=	rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i:=0;i<10;i++  {
+		fmt.Println(r.Int63())
+	}
+
+
+
+}
--- main.go
+++ main.go
@@ -2,42 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
 func main()  {
 
-	//for i:=0;i<10;i++ {
-	//	//生成0-100之间的整数随机数
-	//	fmt.Println(rand.Intn(100))
-	//}
-
-	//初始时间化随机数种子
-	//rand.Seed(time.Now().UnixNano())
-	//for i:=0;i<10;i++  {
-	//	fmt.Println(rand.Intn(100))
-	//}
-
-	//初始时间化随机数种子
-	//r:=	rand.New(rand.NewSource(time.Now().UnixNano()))
-	//for i:=0;i<10;i++  {
-	//	fmt.Println(r.Intn(100))
-	//}
-
-
-	for i:=0;i<10;i++  {
-		fmt.Println(rand.Int63())
-	}
-	//初始化时间随机数种子
-	r:=	rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i:=0;i<10;i++  {
-		fmt.Println(r.Int63())
-	}
-
-
-
-
+	fmt.Print(time.Now())
 
 }
 
