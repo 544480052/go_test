fd27eecee97af774fcf75f2b027d153d046fd3dd
程曦 <544480052@qq.com>
2018-06-08 17:47:13 +0800

    debug

--- main.go
+++ main.go
@@ -5,9 +5,21 @@ import (
 	"time"
 )
 
-func main()  {
+func main() {
 
-	fmt.Print(time.Now().Unix())
+	//毫秒级时间戳
+	fmt.Println(time.Now().UnixNano())
+
+	//秒级时间戳
+	timestamp := time.Now().Unix()
+	fmt.Println(timestamp)
+
+	//yyyy-mm-dd时间格式化成时间戳
+	tm1, _ := time.Parse("YYYY-mm-dd HH:ii:ss", "2018-01-01 15:04:05")
+	fmt.Println(tm1.Unix())
+
+	//时间戳格式化成yyyy-mm-d
+	tm2 := time.Unix(timestamp, 0)
+	fmt.Println(tm2.Format("YYYY-mm-dd HH:ii:ss"))
 
 }
-
