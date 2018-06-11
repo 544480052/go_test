10f6be8b964e65946f1981a719e2b36ed77a2ae9
程曦 <544480052@qq.com>
2018-06-11 18:17:20 +0800

    debug

--- main.go
+++ main.go
@@ -21,21 +21,22 @@ func main() {
 	}
 
 	//执行sql
-	rows, error := db.Query("select id,staff_name from user")
+	rows, error := db.Query("select staff_name from user")
 	defer rows.Close() //方法执行结束之前关闭链接
 	if error != nil {
 		fmt.Println(error)
 	}
 
 	name :=[...]string{}
+	index :=0
 	for rows.Next() {
-		var id int
 		var staff_name string
-		err := rows.Scan(&id, &staff_name)
+		err := rows.Scan(&staff_name)
 		if err != nil {
 			fmt.Println(err)
 		}
-		name[id] = staff_name
+		name[index] = staff_name
+		index++
 	}
 	fmt.Println(name)
 
